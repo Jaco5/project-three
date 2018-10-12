5f153946716685cde98baffc3670131b3a8ddae6
Cool Jake <jacobartz1987@gmail.com>
2018-10-12 15:06:08 -0600

    searchAPI, createTreeData = working, still can't draw tree

---
+++ src/Components/TreeChart/TreeChart.js
@@ -0,0 +1,24 @@
+// import Tree from "../../Pages/Tree"
+// import React, { Component, Fragment } from "react"
+// const google = window.google;
+// google.charts.load('current', { packages: ['wordtree'] });
+
+
+// class TreeChart extends Tree {
+
+//     state = {
+//         treeData : []
+//     }
+
+    
+        
+       
+//         //   google.charts.setOnLoadCallback(drawChart);
+    
+
+
+// render() {
+//    return( 
+//    )}
+// }
+// export default TreeChart;
--- src/Pages/Tree/Tree.js
+++ src/Pages/Tree/Tree.js
@@ -1,133 +1,159 @@
-import {observable} from "mobx"
-import {observer} from "mobx-react"
+// import {observable} from "mobx"
+// import {observer} from "mobx-react"
+import TreeChart from "../../Components/TreeChart"
 import { Input, FormBtn } from "../../Components/Form";
 import React, { Component, Fragment } from "react";
 import API from "../../Utils/API";
 import { Col } from "../../Components/Grid";
+import { Chart } from "react-google-charts";
+// const google = window.google;
+// google.charts.load('current', { packages: ['wordtree'] });
 
-const google = window.google;
-google.charts.load('current', {packages:['wordtree']});
-    //   google.charts.setOnLoadCallback(drawChart);
-    
+const options = {
+    wordtree: {
+      format: "implicit",
+      word: "the"
+    }
+  };
+
 
 class Tree extends Component {
     state = {
-        results= {},
-        treeData= [],
-        node= "",
-        phrase= "",
-        abstract= "",
-        author= "",
-        title= ""
+        results: [],
+        treeData: [],
+        node: "",
+        phrase: "",
+        abstract: "",
+        author: "",
+        title: ""
     }
-    
-    componentDidMount() {
-
-    };
-
-     drawChart = () => {
-        //
-        var data = google.visualization.arrayToDataTable(
-            [this.state.treeData]
-        );
-
-        var options = {
-            wordtree: {
-                format: 'implicit',
-                type: 'double',
-                word: this.node
-            }
-        };
-
-        var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
-        chart.draw(data, options);
-    };
 
 
-    APIsearch = () => {
-        // set state.treedata = empty []
+    APIsearch = (event) => {
+        
+        // this.setState({ treeData: [] })
         API.searchAPI(this.phrase)
-            .then(result => {
+            .then(async result => {
                 console.log("RESULT: ", result);
-                this.setState({ results: result.data.results }, this.createTreeData())
-                
+                await this.setState({ results: result.data.results })
+                console.log("hi wait")
+                this.createTreeData()
             }).catch(e => console.log(e));;
+    }
+
+    createTreeData = () => {
+        let bigArray = []
+        for (var i=0; i<this.state.results.length; i++) {
+            if (this.state.results[i].bibjson.abstract) {
+            let singleArray = [this.state.results[i].bibjson.abstract];
+            console.log(singleArray);
+            bigArray.push(singleArray);
+            }
         }
+        console.log(bigArray);
+        console.log(this.state.results)
+        // this.state.results.map( result => {
+        //     console.log(result)
+        //      this.setState({ treeData: [...this.state.treeData, result.bibjson.abstract] })
+            
+        // })
+        this.state.results.map( result => {
+             this.setState({ treeData: bigArray });  
+        });
 
-     createTreeData = () => {
-
-            for (let i = 0; i < this.state.results.length; i++) {
-                this.treedata.push(this.state.results[i].abstract)
-                this.setState({ treeData: [...this.treeData, this.state.results[i].bibjson.abstract] })
-            }
-            console.log("tree ", this.treeData)
-            // this.drawChart();
-        };
-
-        //call this on click after you have entered a phrase
-        relevantAbstracts = () => this.state.articles.filter(article => article.abstract.includes(this.state.phrase));
-
-
-        handleInputChange = event => {
-            const { name, value } = event.target;
-            this.setState({
-                [name]: value
-            });
-        };
-
-        handleAPI = event => {
-            event.preventDefault();
-            if (this.state.phrase) {
-                this.APIsearch();
-            };
-        };
-
-        handleSaveBtn = event => {
-            if (this.state.title && this.state.author && this.state.abstract) {
-                API.saveArticle({
-                    title: this.state.title,
-                    author: this.state.author,
-                    synopsis: this.state.abstract
-                })
-                    .then(res => alert("Article archived"))
-                    .catch(err => console.log(err));
-            }
-        };
+        setTimeout(() => {
+            console.log(this.state)
+        }, 5000);
 
 
-        render() {
-            return (
-                <Fragment>
-                    <Col size="col-6">
-                        {/* search bars for GS and for resultsObj search */}
-                        <form>
-                            <Input
-                                value={this.state.node}
-                                onChange={this.handleInputChange}
-                                name="node"
-                                placeholder="This is the node of the tree (required)"
-                            />
-                            <Input
-                                value={this.state.phrase}
-                                onChange={this.handleInputChange}
-                                name="phrase"
-                                placeholder="Enter your search term (required)"
-                            />
-                            <FormBtn
-                                disabled={!(this.state.phrase)}
-                                onClick={this.handleAPI}
-                            >
-                                Google Scholar
-                            </FormBtn>
-                        </form>
-                    </Col>
-                    <Col size="col-6">
-                        {/* display the article from resultsObj search w/a save button */}
-                    </Col>
-                    <div id="wordtree_basic" style={{ width: 100 + '%', height: 500 + "px" }}></div>
-                </Fragment>
-            )
+    }
+
+
+    //call this on click after you have entered a phrase
+    relevantAbstracts = () => this.state.results.filter(article => article.abstract.includes(this.state.phrase));
+
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleAPI = event => {
+        event.preventDefault();
+        if (this.state.phrase) {
+            this.APIsearch();
         };
     };
 
-    export default Tree;
+    handleSaveBtn = event => {
+        if (this.state.title && this.state.author && this.state.abstract) {
+            API.saveArticle({
+                title: this.state.title,
+                author: this.state.author,
+                synopsis: this.state.abstract
+            })
+                .then(res => alert("Article archived"))
+                .catch(err => console.log(err));
+        }
+    };
+
+   
+
+
+    render() {
+        return (
+            <Fragment>
+                <Col size="col-6">
+                    {/* search bars for GS and for resultsObj search */}
+                    <form>
+                        <Input
+                            value={this.state.node}
+                            onChange={this.handleInputChange}
+                            name="node"
+                            default="The"
+                        // placeholder="This is the node of the tree (required)"
+                        />
+                        <FormBtn
+                            disabled={!(this.state.phrase)}
+                            onClick={this.relevantAbstracts}
+                        >
+                            Find Article
+                            </FormBtn>
+                        <Input
+                            value={this.state.phrase}
+                            onChange={this.handleInputChange}
+                            name="phrase"
+                            placeholder="Enter your search term (required)"
+                        />
+                        <FormBtn
+                            disabled={!(this.state.phrase)}
+                            onClick={this.handleAPI}
+                        >
+                            Google Scholar
+                            </FormBtn>
+                    </form>
+                </Col>
+                <Col size="col-6">
+                    {/* display the article from resultsObj search w/a save button */}
+                </Col>
+                <div className={"my-pretty-chart-container"}>
+                    <Chart
+                        chartType="WordTree"
+                        data={this.state.treedata}
+                        width="100%"
+                        height="400px"
+                        options= {this.options}
+                          
+                          
+                        legendToggle
+                    />
+                </div>
+                <div id="wordtree_basic" style={{ width: 100 + '%', height: 500 + "px" }}></div>
+            </Fragment>
+        )
+    };
+};
+
+export default Tree;
