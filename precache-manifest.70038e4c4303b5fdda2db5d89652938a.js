4e8cffa74c252ec82eefacfaa0e355fb2dbc6d0a
Cool Jake <jacobartz1987@gmail.com>
2018-10-15 14:33:35 -0600

    Updates

---
+++ precache-manifest.70038e4c4303b5fdda2db5d89652938a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89535f7d38a33e815770",
+    "url": "/project-three/static/js/runtime~main.89535f7d.js"
+  },
+  {
+    "revision": "65d3e7fda263c39bc056",
+    "url": "/project-three/static/js/main.65d3e7fd.chunk.js"
+  },
+  {
+    "revision": "1af5823a52cff6f23094",
+    "url": "/project-three/static/js/1.1af5823a.chunk.js"
+  },
+  {
+    "revision": "65d3e7fda263c39bc056",
+    "url": "/project-three/static/css/main.5656810a.chunk.css"
+  },
+  {
+    "revision": "d0aa1e9bcdea1598f3462fb05cb16a59",
+    "url": "/project-three/index.html"
+  }
+];
